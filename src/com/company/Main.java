4bd59b7dd7f1509ed70eff3c79b500306ba8a111
Roy Teneij <411850@student.fontys.nl>
2020-09-24 04:27:06 +0200

    opdracht 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,32 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        // outOfMemory();
+        // stackOverflow();
+        opdracht1C_2();
+    }
+
+    static void outOfMemory() {
+        long[] l = new long[Integer.MAX_VALUE];
+    }
+
+    static void stackOverflow() {
+        stackOverflow();
+    }
+
+    static void opdracht1C_1() {
+        // Eerst deel je 1 door 42 en daarna vermenigvuldig je 42 met de uitkomst waardoor je weer terug komt op 1 of 1.0
+        // Daarom geeft dit true.
+
+        int i = 42;
+        float f = 1.0f / i;
+        System.out.println((i*f==1.0f));
+    }
+
+    static void opdracht1C_2() {
+        int i = 41;
+        float f = 1.0f / i;
+        System.out.println((i*f==1.0f));
     }
 }
